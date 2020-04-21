61163841c7afbac28adfb1e25a08ddd0f80a2a5d
apple <2543849763@qq.com>
2020-04-21 18:22:47 +0800

    test

--- factory-method/src/test/java/me/zbl/factory/my/App.java
+++ factory-method/src/test/java/me/zbl/factory/my/App.java
@@ -1,11 +1,19 @@
 package me.zbl.factory.my;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
     public static void main(String[] args) {
 
+
+
+
         NumberFactory factory = NumberFactory.getFactory();
+
         System.out.println(factory.parse("122.0"));
+
 
     }
 }
